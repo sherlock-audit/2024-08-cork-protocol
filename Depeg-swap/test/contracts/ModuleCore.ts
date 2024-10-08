7e0dc4886992d3c2f770e25c48d4f6d1c6cad52a
sherlock-admin <dev@sherlock.xyz>
2024-10-08 18:17:56 +0000

    Fix Review

--- Depeg-swap/test/contracts/ModuleCore.ts
+++ Depeg-swap/test/contracts/ModuleCore.ts
@@ -472,6 +472,7 @@ describe("Module Core", function () {
         true,
         true,
         true,
+        true,
       ]);
       const events = await moduleCore.getEvents.PoolsStatusUpdated({
         Id: fixture.Id,
